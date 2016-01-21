3b8be008229b236246ae4a1674c9a13b8f602e38
hpeffley <peffhenry@verizon.net>
2016-01-21 10:26:48 -0500

    Version 0.1 Jimmy John's menu

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -18,7 +18,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         char option;
-
+        double price = 0;
 
         System.out.println("Welcome to Jimmy John's\nWhat would you like?");
         do {
@@ -28,11 +28,50 @@ public class Main {
             System.out.println("(S)alami");
             System.out.println("(T)una");
             System.out.println("(D)one");
+            System.out.println("Enter you choice: ");
 
-            //Get the user input
+
+            //Get the user input adn convert to upper case
             String choice = input.next();
+            choice = choice.toUpperCase();
+
             //Store the first letter in a char
             option = choice.charAt(0);
+
+            // Switch based on the option
+            switch (option) {
+                //Ham
+                case 'H':
+                    price += 6.50;
+                    break;
+
+                //Roast Beef
+                case 'R':
+                    price += 6.75;
+                    break;
+
+                //Salami
+                case 'S':
+                    price += 6.25;
+                    break;
+
+                //Tuna
+                case 'T':
+                    price += 5.50;
+                    break;
+
+                //done
+                case 'D':
+                    price += 0;
+                    break;
+
+                default:
+                    System.out.println("Sorry " + option + " is not available");
+                    price += 0;
+            }
+
+            System.out.println("You chose: " + choice);
+            System.out.println("Cost: $" + price + "0");
 
         } while (option != 'D');
         System.out.println("Thank you for choosing Jimmy John's");
